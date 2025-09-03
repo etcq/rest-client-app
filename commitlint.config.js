0a1fab5debc446654357179161d45855d169c8d4
etcq <resonans93@gmail.com>
2025-09-04 00:20:33 +0500

    feat: add commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,23 @@
-export default { extends: ['@commitlint/config-conventional'] };
+const Configuration = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'ci',
+        'init',
+        'feat',
+        'fix',
+        'refactor',
+        'chore',
+        'style',
+        'docs',
+        'test',
+      ],
+    ],
+  },
+};
+
+export default Configuration;
