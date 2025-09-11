766e9a2499313edac6f70ed67a9cf58954c23a3f
turik777 <52492191+turik777@users.noreply.github.com>
2025-09-11 04:53:03 +0300

    feat: add onSubmit handler with redirect

--- src/components/example/auth-form/auth-form.tsx
+++ src/components/example/auth-form/auth-form.tsx
@@ -5,6 +5,9 @@ import { type FieldErrors, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { TextField, Button, Box, Typography, Link } from '@mui/material';
 import { type SignInInput, type SignUpInput, signInSchema, signUpSchema } from '@/schema/auth-schema';
+import { loginWithCredentials } from '@/actions/sign-in';
+import { registerUser } from '@/actions/register';
+import { redirect } from 'next/navigation';
 
 interface IProps {
   type: 'login' | 'register';
@@ -18,20 +21,27 @@ const AuthForm: FC<IProps> = ({ type }) => {
 
   const {
     register,
+    handleSubmit,
     formState: { errors, isValid },
   } = useForm<TFormValues>({
     resolver: zodResolver(schema),
     mode: 'onChange',
   });
 
+  const onSubmit = async (formData: TFormValues) => {
+    if (isLogin) {
+      const { email, password } = formData as SignInInput;
+      await loginWithCredentials(email, password);
+    } else {
+      await registerUser(formData as SignUpInput);
+      const { email, password } = formData as SignUpInput;
+      await loginWithCredentials(email, password);
+    }
+    redirect('/');
+  };
+
   return (
-    <Box
-      component="form"
-      sx={{ width: 400, mx: 'auto', mt: 8 }}
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
-    >
+    <Box component="form" sx={{ width: 400, mx: 'auto', mt: 8 }} onSubmit={handleSubmit(onSubmit)}>
       <Typography variant="h5" align="center" mb={2}>
         {isLogin ? 'Sign In' : 'Sign Up'}
       </Typography>
@@ -73,7 +83,7 @@ const AuthForm: FC<IProps> = ({ type }) => {
       </Box>
 
       <Box sx={{ textAlign: 'center' }}>
-        <Link href="#" variant="body1" sx={{ display: 'block', mt: 2 }}>
+        <Link href={isLogin ? '/registration' : '/login'} variant="body1" sx={{ display: 'block', mt: 2 }}>
           {isLogin ? "Don't have an account? Sign Up" : 'Already have an account? Sign In'}
         </Link>
       </Box>
