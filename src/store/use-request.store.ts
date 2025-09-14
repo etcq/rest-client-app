e2ada51a77c1ddbc057a8d88ed3feb0e5337e622
Siarhei Buiko <innxfect@gmail.com>
2025-09-14 14:38:50 +0300

    refactor(interface): extract IRequestData/IHeader to types/interfaces.ts

--- src/store/use-request.store.ts
+++ src/store/use-request.store.ts
@@ -1,16 +1,12 @@
 import { create } from 'zustand';
-
-export interface IHeader {
-  id: string;
-  key: string;
-  value: string;
-}
+import type { IHeader } from '@interfaces';
 
 interface IRequestState {
   headers: IHeader[];
   addHeader: () => void;
   updateHeader: (id: string, newKey: string, newValue: string) => void;
   removeHeader: (id: string) => void;
+  setHeaders: (newHeaders: IHeader[]) => void;
 }
 
 const useRequestStore = create<IRequestState>((set) => ({
@@ -24,19 +20,23 @@ const useRequestStore = create<IRequestState>((set) => ({
 
   updateHeader: (id, newKey, newValue): void => {
     set((state) => ({
-      headers: state.headers.map((header: IHeader) =>
-        header.id === id ? { ...header, key: newKey, value: newValue } : header
+      headers: state.headers.map(
+        (header: IHeader): IHeader => (header.id === id ? { ...header, key: newKey, value: newValue } : header)
       ),
     }));
   },
 
   removeHeader: (id): void =>
     set((state) => {
-      const headers = state.headers.filter((header: IHeader): boolean => header.id !== id);
+      const headers: IHeader[] = state.headers.filter((header: IHeader): boolean => header.id !== id);
       return {
         headers: headers.length ? headers : [{ id: crypto.randomUUID(), key: '', value: '' }],
       };
     }),
+
+  setHeaders: (newHeaders: IHeader[]): void => {
+    set({ headers: newHeaders });
+  },
 }));
 
 export default useRequestStore;
