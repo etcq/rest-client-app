adbf685eb28439525c0c33057895d03a6345f7da
Siarhei Buiko <innxfect@gmail.com>
2025-09-18 21:48:49 +0300

    refactor(utils): remove unused converter

--- src/utils/headers.ts
+++
@@ -1,9 +0,0 @@
-import type { IHeader } from '@interfaces';
-
-export const convertHeadersArrayToObject = (headers: IHeader[]): Record<string, string> => {
-  return Object.fromEntries(
-    headers
-      .filter((header: IHeader): string => header.key.trim())
-      .map((header: IHeader): [string, string] => [header.key, header.value])
-  );
-};
