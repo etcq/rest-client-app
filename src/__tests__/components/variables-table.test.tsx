40c1308e0d5925ff5f4b2c699d06a2cbfb19cea8
etcq <resonans93@gmail.com>
2025-09-18 22:58:55 +0500

    test: add test for vars page and custom variables storage hook

--- src/__tests__/components/variables-table.test.tsx
+++ src/__tests__/components/variables-table.test.tsx
@@ -1,5 +1,5 @@
 import { expect, describe, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { NextIntlClientProvider } from 'next-intl';
 import messagesEn from '../../../messages/en.json';
 import VariablesTable from '@components/variables-table/variables-table';
@@ -14,5 +14,24 @@ describe('Variables table component', () => {
     expect(screen.getByText('Variables Key')).toBeInTheDocument();
     expect(screen.getByText('Variables Value')).toBeInTheDocument();
     expect(screen.getByText('Controls')).toBeInTheDocument();
+  });
+  it('Form should added new rows in table', () => {
+    render(
+      <NextIntlClientProvider locale="en" messages={messagesEn}>
+        <VariablesTable />
+      </NextIntlClientProvider>
+    );
+    const keyField = screen.getByLabelText(/Key/i);
+    const valueField = screen.getByLabelText(/value/i);
+    const button = screen.getByText(/add/i);
+
+    fireEvent.input(keyField, { target: { value: 'testKey' } });
+    fireEvent.input(valueField, { target: { value: 'testValue' } });
+    fireEvent.click(button);
+
+    waitFor(() => {
+      expect(screen.getByText('testKey')).toBeInTheDocument();
+      expect(screen.getByText('testValue')).toBeInTheDocument();
+    });
   });
 });
