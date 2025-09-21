5cc94b90f2417982ce0ec3e5b3ebcf7773ca025a
etcq <resonans93@gmail.com>
2025-09-21 15:33:40 +0500

    feat: add convert vars for restful page

--- src/app/[locale]/restful-client/page.tsx
+++ src/app/[locale]/restful-client/page.tsx
@@ -16,6 +16,7 @@ import CodeTab from '@/components/tabs/code';
 import { useLocale } from 'next-intl';
 import { usePathname, useSearchParams } from 'next/navigation';
 import useRequestHistoryStore from '@store/request-history';
+import useVariablesStorage from '@/hooks/use-variables-storage';
 
 export default function RestfulClient() {
   const [method, setMethod] = useState<METHODS>(METHODS.GET);
@@ -23,6 +24,7 @@ export default function RestfulClient() {
   const [body, setBody] = useState<string>('');
   const [loading, setLoading] = useState(false);
   const [responseData, setResponseData] = useState<{ status?: number; body?: string } | null>(null);
+  const { convert } = useVariablesStorage();
 
   const locale = useLocale();
   const pathname = usePathname();
@@ -145,7 +147,7 @@ export default function RestfulClient() {
           fullWidth
           placeholder={t('placeholders.url')}
           value={url}
-          onChange={(e) => setUrl(e.target.value)}
+          onChange={(e) => setUrl(convert(e.target.value))}
           error={urlError}
           helperText={urlError ? t('errors.incorrectUrl') : ' '}
           sx={{ maxWidth: '600px' }}
--- src/components/headers-row/index.tsx
+++ src/components/headers-row/index.tsx
@@ -3,6 +3,7 @@ import { useTranslations } from 'next-intl';
 import type { IHeader } from '@interfaces';
 import useRequestStore from '@store/use-request.store';
 import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
+import useVariablesStorage from '@/hooks/use-variables-storage';
 
 export interface IHeadersRowProps {
   header: IHeader;
@@ -10,6 +11,7 @@ export interface IHeadersRowProps {
 
 const HeadersRow = ({ header }: IHeadersRowProps) => {
   const t = useTranslations('restfulPage');
+  const { convert } = useVariablesStorage();
   const { updateHeader, removeHeader } = useRequestStore();
   return (
     <Box sx={{ display: 'flex', gap: '5px', alignItems: 'center', mb: '5px' }}>
@@ -17,13 +19,13 @@ const HeadersRow = ({ header }: IHeadersRowProps) => {
         size={'small'}
         value={header.key}
         placeholder={t('placeholders.headers.key')}
-        onChange={(event) => updateHeader(header.id, event.target.value, header.value)}
+        onChange={(event) => updateHeader(header.id, convert(event.target.value), header.value)}
       ></TextField>
       <TextField
         size={'small'}
         value={header.value}
         placeholder={t('placeholders.headers.value')}
-        onChange={(event) => updateHeader(header.id, header.key, event.target.value)}
+        onChange={(event) => updateHeader(header.id, header.key, convert(event.target.value))}
       ></TextField>
       <DeleteForeverOutlinedIcon sx={{ ml: '10px', cursor: 'pointer' }} onClick={() => removeHeader(header.id)} />
     </Box>
