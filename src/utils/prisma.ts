e24f8e6125859a6dd79eaaeed62b9e0d095281cc
etcq <resonans93@gmail.com>
2025-09-24 04:49:04 +0500

    fix: move another version prisma

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -6,4 +6,4 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-export default prisma;
+export { prisma };
